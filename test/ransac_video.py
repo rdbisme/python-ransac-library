615ae84d798a3f17aa92576ee3e9bdc20c1115dc
Ruben Di Battista <rubendibattista@gmail.com>
2015-06-09 13:35:36 +0200

    Changed tests for other video.

--- test/ransac_video.py
+++ test/ransac_video.py
@@ -1,5 +1,10 @@
 import tra.ransac as rns
 import tra.features as fts
+import numpy as n
 
-ransac = rns.RansacFeature(fts.Circle)
-circles = ransac.video_processing('../video/01_CMP.avi')
+ransac = rns.RansacFeature(fts.Circle,max_it=100,threshold=80,dst=1)
+circles = ransac.video_processing('../video/02_XVID.avi')
+
+circles = [[c.radius,c.xc,c.yc] for c in circles]
+
+n.save('data.npy',circles)
--- test/ransac_video_output.py
+++ test/ransac_video_output.py
@@ -1,15 +1,30 @@
+from __future__ import division
 import numpy as n
 import scipy.signal as sig
 import matplotlib.pyplot as plt
+import scipy.optimize as opt
+
+pixel_to_mm = 8/350
+frame_to_time = 1/250
 
 circles = n.load('data.npy')
 circles = circles[circles[:,0]>70]
+circles = circles[circles[:,0]<500]
 
 b,a = sig.butter(8,0.01, 'low', output='ba')
-filtered_circles = sig.filtfilt(b, a, circles[:,0])
+filtered = sig.filtfilt(b, a, circles[:,0])
 
-plt.plot(circles[:,0],'-.',mfc='none')
-plt.plot(filtered_circles,'r-',linewidth=5)
+def opt_func(x,a,b,n):
+    return a*x**n + b
+
+x = range(n.size(filtered))
+
+popt,_P = opt.curve_fit(opt_func, x, circles[:,0])
+y = opt_func(x,*popt)
+
+plt.plot(x,circles[:,0],'-.',mfc='none')
+#plt.plot(x,filtered,'g-',linewidth=5)
+plt.plot(x,y,'r-')
 plt.xlabel('Frames No.')
 plt.ylabel('Radius [pixels]')
 plt.show()
