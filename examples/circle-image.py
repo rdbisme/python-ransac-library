4421e877a04ef34b5736d4d19a77874dc988c36f
Ruben Di Battista <rubendibattista@gmail.com>
2015-11-30 03:20:38 +0100

    Adapt examples to commit #f13a2ab

--- examples/circle-image.py
+++ examples/circle-image.py
@@ -7,14 +7,14 @@ from pyransac.features import Circle
 
 # Extract a frame from the provided video
 video = cv2.VideoCapture('media/HRE-video.avi')
-video.set(cv2.CAP_PROP_POS_FRAMES,250)
+video.set(cv2.CAP_PROP_POS_FRAMES,70)
 succ, frame = video.read()
 
 # Set Gray Color
 frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
 # Init RansacFeature class for circle detection
-ransac_process = RansacFeature(Circle,max_it = 100, inliers_percent=0.7,dst=1,threshold=100)
+ransac_process = RansacFeature(Circle,max_it = 100, inliers_percent=0.7,dst=2,threshold=100)
 
 # Process the frame to detect the circle
 dc,percent = ransac_process.image_search(frame)
@@ -23,6 +23,15 @@ dc,percent = ransac_process.image_search(frame)
 
 theta = n.linspace(-n.pi,n.pi,100)
 plt.imshow(frame, cmap='gray')
-plt.plot(dc.yc + dc.radius*n.cos(theta), dc.xc + dc.radius*n.sin(theta),'r-',linewidth=2)
+x,y = dc.print_feature(100)
+
+# Reverted x,y because 
+# OpenCV uses numpy.ndarray
+# to store images and
+# image[:,0] is the vertical
+# axis of it. 
+# See: http://stackoverflow.com/q/30681997/1334711
+
+plt.plot(y,x,'r-',linewidth=2)
 plt.axis('off')
 plt.show()
--- examples/exponential.py
+++ examples/exponential.py
@@ -1,22 +1,50 @@
+from __future__ import print_function
 import numpy as np
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
 from pyransac import features as ffs
+
+# Generating an exponential function
+start = 2
+end = 10
+num_points = 11
+
 a = 1
 b = 1
 n = 3
-x = np.linspace(2,10,11)
+x = np.linspace(a,10,11)
 y = a*np.power(x,n)+b
+
+# Select three points from the mathematical
+# function
 i = [0,3,5]
 points = np.array([
                    pps for pps in zip(x[i],y[i])
                    ])
                    
-print points
 
+# Generate Exponential feature from the 
+# three points
 exp = ffs.Exponential(points)
-p = [exp.a,exp.k,exp.b]
+
+# Compare parameters
+print("""
+      Manual set parameters for exponential y=ax*n + b:\n
+      a = {0}
+      b = {1}
+      k = {2}
+      
+      Detected feature parameters:
+      
+      a = {3}
+      b = {4}
+      k = {5}
+      """.format(a,b,n,exp.a,exp.b,exp.k)
+      )
+
+# Print feature
+xe,ye = exp.print_feature(num_points,start,end)
 plt.plot(x,y)
 plt.plot(x[i],y[i],'*')
-plt.plot(x,p[0]*np.power(x,p[1])+p[2],'o-')
+plt.plot(xe,ye,'o-')
 plt.show()
